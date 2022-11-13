e178fa946b2b0fc36d5045870e346f27dba3a756
krzysiekbab <krzysiek_bab@wp.pl>
2022-11-13 19:21:18 +0100

    Deleting ProcessAudio class; adding its functionality to Audio class; clearing main.py file

--- audio_class.py
+++ audio_class.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from playsound import playsound
 from constants import *
 import matplotlib.collections as collections
+import os
 
 
 class Audio:
@@ -22,24 +23,12 @@ class Audio:
     def load_audio(self):
         self.audio_data, self.sr = librosa.load(path=self.audio_path, sr=SAMPLE_RATE)
 
-    def plot_wave(self):
-        plt.figure()
-        librosa.display.waveshow(self.audio_data, alpha=0.5)
-        plt.title(f"Wave plot of {self.audio_path}")
-        plt.ylim(-1, 1)
-        plt.show()
-
     def play_audio(self):
         print(f"Playing {self.audio_name} ...")
         playsound(self.audio_path)
 
-
-class ProcessAudio:
-    def __init__(self, audio_file: Audio):
-        self.audio_file = audio_file
-
     def detect_onsets(self, units='samples'):
-        o_env = librosa.onset.onset_strength(y=self.audio_file.audio_data, sr=SAMPLE_RATE)
+        o_env = librosa.onset.onset_strength(y=self.audio_data, sr=SAMPLE_RATE)
         onsets = librosa.onset.onset_detect(onset_envelope=o_env, sr=SAMPLE_RATE, units=units)
         return onsets
 
@@ -48,18 +37,36 @@ class ProcessAudio:
         framed_signal = []
         for i in range(len(onsets)):
             if i == len(onsets) - 1:
-                framed_signal.append(self.audio_file.audio_data[onsets[i]:self.audio_file.audio_data.shape[0]])
+                framed_signal.append(self.audio_data[onsets[i]:self.audio_data.shape[0]])
             else:
-                framed_signal.append(self.audio_file.audio_data[onsets[i]:onsets[i + 1]])
-        # return np.array(framed_signal, dtype=object)
+                framed_signal.append(self.audio_data[onsets[i]:onsets[i + 1]])
         return framed_signal
+
+    def find_sound_and_silence_ranges(self, onset_frames, onsets, top_db):
+        note_duration = []
+        silence_duration = []
+        indexes = np.insert(onsets, 0, 0)
+        indexes = np.insert(indexes, len(indexes), self.audio_data.shape[0])
+        for i in range(len(onset_frames)):
+            onset_frame_splitted = librosa.effects.split(onset_frames[i], top_db=top_db) + indexes[i + 1]
+
+            sound = onset_frame_splitted[0]  # omijamy przypadek gdy wykrywa drugą wartość zwykle przed końcem ramki.
+            # Rozwiązuje to problemwzrastających wartości na końcu ramki
+
+            note_duration.append(sound)
+            silence = np.array([onset_frame_splitted[0][1], indexes[i + 2]])
+            silence_duration.append(silence)
+        return note_duration, silence_duration
 
     @staticmethod
     def divide_onset_frame_into_smaller_frames(data_frame, window_size=WINDOW_SIZE, hop_length=HOP_LENGTH):
         # TODO zmienić to na lepszy sposób dopasowania
-        if len(data_frame) < window_size:
+        if len(data_frame) <= window_size:
             window_size = int(window_size / 4)
-            hop_length = int(hop_length / 4)
+            # hop_length = int(hop_length / 4)
+        if len(data_frame) <= 2*window_size:
+            window_size = int(window_size / 2)
+            # hop_length = int(hop_length / 2)
 
         smaller_frames = librosa.util.frame(data_frame, frame_length=window_size, hop_length=hop_length, axis=0)
         # multiplying frame by Hanning window
@@ -100,48 +107,77 @@ class ProcessAudio:
             first_idx, second_idx = notes_durations[i]
 
             # dividing every onset_frame into smaller frames
-            frames = self.divide_onset_frame_into_smaller_frames(self.audio_file.audio_data[first_idx:second_idx])
-
+            frames = self.divide_onset_frame_into_smaller_frames(self.audio_data[first_idx:second_idx])
             f0_candidate = []
             for frame in frames:
                 f0_freq = self.find_f0_frequency(frame)
                 f0_candidate.append(f0_freq)
+            # print(f"Frame[{i}], len={len(f0_candidate)}", np.sort(f0_candidate))
             f0_freq = np.median(f0_candidate)
             found_frequencies.append(f0_freq)
         return np.array(found_frequencies)
 
-    @staticmethod
-    def find_sound_and_silence_ranges(onset_frames, indexes, top_db):
-        note_duration = []
-        silence_duration = []
-        for i in range(len(onset_frames)):
-            onset_frame_splitted = librosa.effects.split(onset_frames[i], top_db=top_db) + indexes[i + 1]
+    def plot_wave(self):
+        plt.figure()
+        librosa.display.waveshow(self.audio_data, alpha=0.5, sr=SAMPLE_RATE)
+        plt.title(f"Wave plot of {self.audio_path}")
+        plt.show()
 
-            sound = onset_frame_splitted[0]  # omijamy przypadek gdy wykrywa drugą wartość zwykle przed końcem ramki.
-            # Rozwiązuje to problemwzrastających wartości na końcu ramki
+    def plot_magnitude_spectrum(self, f_ratio=0.1):
+        X = np.fft.fft(self.audio_data)
+        X_mag = np.abs(X)
+        plt.figure(figsize=(10, 8))
 
-            note_duration.append(sound)
-            silence = np.array([onset_frame_splitted[0][1], indexes[i + 2]])
-            silence_duration.append(silence)
-        return note_duration, silence_duration
+        f = np.linspace(0, self.sr, len(X_mag))
+        f_bins = int(len(X_mag) * f_ratio)
+        print(np.argmax(f[:f_bins]))
+        plt.plot(f[:f_bins], X_mag[:f_bins])
+        plt.xlabel('Częstotliwość (Hz)')
+        plt.title(self.audio_name)
+        plt.show()
+
+    def plot_spectrogram(self):
+        stft_data = np.abs(librosa.stft(self.audio_data))
+        stft_data_db = librosa.amplitude_to_db(stft_data)
+        fig, ax = plt.subplots()
+        img = librosa.display.specshow(stft_data_db, x_axis='time', y_axis='log', ax=ax)
+        ax.set_title(f"Spectrogram: {self.audio_name}")
+        fig.colorbar(img, ax=ax, format="%+2.0f dB")
+        plt.show()
+
+    def plot_mel_spectrogram(self):
+        s = librosa.feature.melspectrogram(y=self.audio_data, sr=self.sr, n_mels=128)
+        s_db_mel = librosa.amplitude_to_db(s, ref=np.max)
+
+        fig, ax = plt.subplots()
+        img = librosa.display.specshow(s_db_mel, x_axis='time', y_axis='log', ax=ax)
+        ax.set_title(f"Mel Spectrogram: {self.audio_name}")
+        fig.colorbar(img, ax=ax, format="%+2.0f dB")
+        plt.show()
 
     def plot_notes_ranges(self, notes_durations, top_db):
-        x = np.arange(0, self.audio_file.audio_data.shape[0])
-        zeros = np.zeros(self.audio_file.audio_data.shape[0])
+
+        x = np.arange(0, self.audio_data.shape[0])/SAMPLE_RATE
+        zeros = np.zeros(self.audio_data.shape[0])
         fig, ax = plt.subplots()
 
         for sound in notes_durations:
             first_idx, second_idx = sound
             zeros[first_idx:second_idx] = 1
         collection = collections.BrokenBarHCollection.span_where(
-            x, ymin=0, ymax=np.abs(self.audio_file.audio_data).max(),
-            where=zeros > 0, facecolor='green',
+            x, ymin=0, ymax=np.abs(self.audio_data).max(),
+            where=zeros > 0, facecolor='orange',
             label='Obszar występowania dźwięku')
         ax.add_collection(collection)
-        ax.plot(self.audio_file.audio_data, color="#ffa500")
-        ax.set_xlabel('Próbki')
+        librosa.display.waveshow(self.audio_data, sr=SAMPLE_RATE)
+        # ax.plot(self.audio_data, color="#ffa500")
+        # ax.plot(self)
+        ax.set_xlabel('Czas [s]')
         ax.set_ylabel('Amplituda')
         ax.set_title(f'Przedziały występowania dźwięków (threshold = {top_db})')
-        ax.legend()
+        ax.legend(loc='lower right')
         plt.show()
 
+
+if __name__ == "__main__":
+    pass
--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from playsound import playsound
 import os
 from constants import *
 from functions import *
-from audio_classes import Audio, ProcessAudio
-import matplotlib.collections as collections
+from audio_class import Audio
 import music21
 
 """
@@ -28,53 +27,11 @@ import music21
 """
 
 
-# class Audio:
-#     def __init__(self, audio_name: str, audio_data: np.ndarray, sr: int):
-#         self.audio_name = audio_name
-#         self.audio_data = audio_data
-#         self.sr = sr
-#     def calculate_short_fourier_transform(self):
-#         return np.abs(librosa.stft(self.audio_data))
-#
-#     def amplitude_to_db_transform(self):
-#         return librosa.amplitude_to_db(self.calculate_short_fourier_transform())
-#
-#     def plot_magnitude_spectrum(self, f_ratio=0.1):
-#         X = np.fft.fft(self.audio_data)
-#         X_mag = np.abs(X)
-#         plt.figure(figsize=(10, 5))
-#
-#         f = np.linspace(0, self.sr, len(X_mag))
-#         f_bins = int(len(X_mag) * f_ratio)
-#         print(np.argmax(f[:f_bins]))
-#         plt.plot(f[:f_bins], X_mag[:f_bins])
-#         plt.xlabel('Frequency (Hz)')
-#         plt.title(self.audio_name)
-#         plt.show()
-#
-#     def plot_spectrogram(self):
-#         fig, ax = plt.subplots()
-#         img = librosa.display.specshow(self.amplitude_to_db_transform(), x_axis='time', y_axis='log', ax=ax)
-#         ax.set_title(f"Power spectrogram\nFile:{self.audio_name}")
-#         fig.colorbar(img, ax=ax, format="%+2.0f dB")
-#         plt.show()
-#
-#     def plot_mel_spectrogram(self):
-#         s = librosa.feature.melspectrogram(y=self.audio_data, sr=self.sr, n_mels=128)
-#         s_db_mel = librosa.amplitude_to_db(s, ref=np.max)
-#
-#         fig, ax = plt.subplots(figsize=(10, 5))
-#         img = librosa.display.specshow(s_db_mel, x_axis='time', y_axis='log', ax=ax)
-#         ax.set_title(f"Mel Spectrogram\nFile: {self.audio_name}")
-#         fig.colorbar(img, ax=ax, format="%+2.0f dB")
-#         plt.show()
-
-
 def main():
     # SET PATH OF AUDIO FILE
     directory = os.getcwd()
-    audio_name = '\Audio\A blues scale with breaks.wav'
-    # audio_name = '\Audio\A blues scale.wav'
+    # audio_name = '\Audio\A blues scale with breaks.wav'
+    audio_name = '\Audio\A blues scale.wav'
     # audio_name = '\Audio\C ionian scale.wav'
     # audio_name = '\Audio\\piano single notes\piano A4.wav'
 
@@ -83,41 +40,41 @@ def main():
 
     # CREATE INSTANCE OF "Audio" CLASS
     audio_file = Audio(audio_path=audio_path, audio_name=audio_name)
-    onset_env = librosa.onset.onset_strength(y=audio_file.audio_data, sr=SAMPLE_RATE)
+    # DETECT ONSETS IN AUDIO FILE
+    onsets = audio_file.detect_onsets()
+    # DIVIDE SIGNAL INTO FRAMES FROM ONSET TO ONSET;
+    onset_frames = audio_file.divide_into_onset_frames(onsets=onsets)
+    # # PICK TOP_DB VALUE
+    top_db = 40
 
-    # CREATE INSTANCE OF "ProcessAudio" CLASS
-    process_audio = ProcessAudio(audio_file=audio_file)
-    onsets = process_audio.detect_onsets()
-
-    # CREATE INDEX ARRAY (ONSETS WITH ADDED INDEXES OF FIRST AND LAST SAMPLE)
-    indexes = np.insert(onsets, 0, 0)
-    indexes = np.insert(indexes, len(indexes), audio_file.audio_data.shape[0])
-
-    # DIVIDE SIGNAL INTO FRAMES FROM ONSET TO ONSET; RETURNS PARTS OF SIGNAL
-    onset_frames = process_audio.divide_into_onset_frames(onsets=onsets)
-
-    # PICK TOP_DB VALUE
-    top_db = 30  # TODO rozwiącać problem gdy zwiększamy do 50 dB, i franctional prat ma stosunkowe małe wartości - rozw.: usuwać dźwięki których długość to 0.
-
-    notes_duration_idx, silences_duration_idx = ProcessAudio.find_sound_and_silence_ranges(onset_frames=onset_frames,
-                                                                                           indexes=indexes,
-                                                                                           top_db=top_db)
-
-    # PLOT TIME SLOTS WERE SOUND WERE IDENTIFIED
-    # process_audio.plot_notes_ranges(notes_durations=notes_duration_idx, top_db=top_db)
+    notes_duration_idx, silences_duration_idx = audio_file.find_sound_and_silence_ranges(onset_frames=onset_frames,
+                                                                                         onsets=onsets,
+                                                                                         top_db=top_db)
+    # # PLOT TIME SLOTS WERE SOUND WERE IDENTIFIED
+    audio_file.plot_notes_ranges(notes_durations=notes_duration_idx, top_db=top_db)
 
     # FIND F0 FREQUENCY
-    found_frequencies = process_audio.find_frequencies(notes_durations=notes_duration_idx)
+    found_frequencies = audio_file.find_frequencies(notes_durations=notes_duration_idx)
     found_frequencies = found_frequencies.round(3)
 
-    # CHANGE FREQUENCY TO MUSIC NAMES
+    print("len(onset_frames): ", len(onset_frames))
+    print("len(notes_duration_idx): ", len(notes_duration_idx))
+    # # CHANGE FREQUENCY TO MUSIC NAMES
     found_notes = change_from_frequency_to_music_notation(found_frequencies)
+    # Printing results
+    # for i in range(len(found_frequencies)):
+    #     print(f"Frame [{i:2}] (), Found frequency: {found_frequencies[i]:.3f} [Hz] is equal to {found_notes[i]}")
 
-    # CREATE LENGTH ARRAYS IN SAMPLE AND TIME UNITS
-    _, _, notes_duration_times, silences_duration_times = create_duration_tables(notes_duration_idx,
-                                                                                 silences_duration_idx)
-    # PICK THE SHORTEST NOTE DURATION IN MUSIC21;
-    shortest_note = '1/32'
+    #
+    # # CREATE LENGTH ARRAYS IN SAMPLE AND TIME UNITS
+    note_duration_samples, _, notes_duration_times, silences_duration_times = create_duration_tables(notes_duration_idx,
+                                                                                                     silences_duration_idx)
+    for i in range(len(found_frequencies)):
+        print(
+            f"Frame [{i:2}] ({note_duration_samples[i]} Samples), Found frequency: {found_frequencies[i]:.3f} [Hz] is "
+            f"equal to {found_notes[i]}")
+    # # PICK THE SHORTEST NOTE DURATION IN MUSIC21;
+    shortest_note = '1/16'
 
     # ROUND NOTE & SILENCE DURATIONS TO MUSIC21 FORMAT; USE THE SHORTEST NOTE DURATION
     notes_duration_times_music21 = change_duration_to_music21_format(duration_times=notes_duration_times,
@@ -143,8 +100,12 @@ def main():
     create_notes_and_rests(music_representation, stream=stream1)
 
     # PRINT STREAM IN MUSESCORE
-    stream1.show()
+    # stream1.show()
 
+    # audio_file.plot_wave()
+    # audio_file.plot_magnitude_spectrum()
+    # audio_file.plot_spectrogram()
+    # audio_file.plot_mel_spectrogram()
     # WRITE SHEETMUSIC TO PDF WITHOUT OPENING MUSESCORE
     # stream1.write('musicxml.pdf', input("Enter The Song Name (Add File Extension): "))
 
@@ -160,10 +121,8 @@ def main():
 if __name__ == "__main__":
     main()
     # LIST OF TODOS:
-
-    # TODO 0: Get rid of Audio class from main.py
     # TODO 1. Write tempo function
-    # TODO 2. Write descriptions and hits of classes and methods in audio_classes.py
+    # TODO 2. Write descriptions and hits of classes and methods in audio_class.py
     # TODO 3. librosa.onset.onset_detect - understand the flow
     # TODO 4. librosa.onset.onset_strength - understand the flow
     # TODO 5. Implement auto correlation method e. g. "librosa.yin" (try to write my own code)
@@ -172,3 +131,4 @@ if __name__ == "__main__":
     # TODO 10. Learn how to generate .exe file to start the application
     # TODO 11. Create GUI
     # TODO 12. Check working of function hz to note by librosa
+    # TODO 13. Manage saving pdf-s to "Results" folder.
