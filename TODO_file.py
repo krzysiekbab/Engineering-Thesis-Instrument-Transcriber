c0968ece3144844900cadf8763aac964fc2621fd
krzysiekbab <krzysiek_bab@wp.pl>
2023-01-17 00:30:53 +0100

    Adding results

---
+++ TODO_file.py
@@ -0,0 +1,15 @@
+# LIST OF TODOS:
+# 1. PROGRAM:
+# TODO: [GUI] - block next button after changing file (need to run algorithm again)
+
+# TODO [Recording] - check in Reaper recording from laptop microphone. Plot spectrum plot of this recording.
+#  Think about filtering these type of audio tracks.
+# TODO: [GUI] - finish Slider functionality
+# TODO: [Github] - Add Readme file on Github -> Make first page look nice
+# TODO: [Code] - Write descriptions and hits of classes and methods in audio_class.py
+# TODO: [General] - Learn how to generate .exe file to start the application
+# TODO: [Code] - Fix error when onset detected is in the end of audio file
+#  and number of samples is shorter than window size
+# TODO: [Code] - Create classes of each page from app.py
+
+# 2. THESIS:
